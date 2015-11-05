05c3c58c242a994a1106d51cf4295605dfc01b4a
Andrew Seward <awswrd@gmail.com>
2015-11-05 15:15:15 -0800

    Quick and dirty cleanup for interview

--- index.js
+++ index.js
@@ -5,30 +5,17 @@ const endpoints = {
   sentences: 'http://liveproxy-rails-example.herokuapp.com/api/v1/sentences',
 }
 
-function _getStoriesPromise() {
-  return new Promise((resolve, reject) => {
-    superagent.get(endpoints.stories).end((err, resp) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(resp.body.stories);
-      }
-    });
-  });
-}
+/*
+ * Notes
+ *
+ * + Each exercise function takes two functions as arguments (cLog and cErr)
+ *   cLog should be used in place of console.log
+ *   cErr should be used in place of console.error
+ */
 
-function _getSentencesPromise(storyId) {
-  return new Promise((resolve, reject) => {
-    superagent.get(endpoints.sentences).query({ story_id: storyId }).end((err, resp) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(resp.body.sentences);
-      }
-    });
-  });
-}
-
+/*
+ * Helpers
+ */
 function __print(fn, exerciseNum, content) {
   fn(`*** Exercise ${exerciseNum} ***`);
   fn(content);
@@ -43,77 +30,46 @@ function _error(exerciseNum, content) {
   __print(console.error, exerciseNum, content);
 }
 
+/*
+ * To implement
+ */
+
+function _getStoriesPromise() {
+  // TODO: Implement
+}
+
+function _getSentencesPromise(storyId) {
+  // TODO: Implement
+}
+
 // Print stories response using callbacks
 function callbackStories(cLog, cErr) {
-  superagent.get(endpoints.stories).end((err, resp) => {
-    if (err) {
-      cErr(err);
-    }
-
-    cLog(resp.body.stories);
-  });
+  // TODO: Implement
 }
 
 // Print the first story's sentences using callbacks
 function callbackFirstStorySentences(cLog, cErr) {
-  superagent.get(endpoints.stories).end((err, resp) => {
-    if (err) {
-      cErr(err);
-    }
-    const stories = resp.body.stories;
-
-    superagent.get(endpoints.sentences).query({ story_id: stories[0].id }).end((err, resp) => {
-      if (err) {
-        cErr(err);
-      }
-
-      cLog(resp.body.sentences);
-    });
-  });
+  // TODO: Implement
 }
 
 // Print stories response using promsies
 function promiseStories(cLog, cErr) {
-  _getStoriesPromise()
-    .then(res => cLog(res))
-    .catch(err => cErr(err));
+  // TODO: Implement
 }
 
 // Print the first story's sentences using promises (naive implementation)
 function promiseFirstStorySentences(cLog, cErr) {
-  _getStoriesPromise()
-    .then(stories => {
-      const firstStory = stories[0];
-      _getSentencesPromise(firstStory.id)
-        .then(sentences => cLog(sentences))
-        .catch(err => cErr(err));
-    })
-    .catch(err => cErr(err));
+  // TODO: Implement
 }
 
 // Print the first story's sentences using promises (flattened implementation)
 function arePromisesReallyBetterThanCallbacks(cLog, cErr) {
-  _getStoriesPromise()
-    .then(stories => {
-      const firstStory = stories[0];
-      return _getSentencesPromise(firstStory.id);
-    })
-    .then(sentences => cLog(sentences))
-    .catch(err => cErr(err));
+  // TODO: Implement
 }
 
 // Print all the stories' sentences
 function promisesAllStoriesSentences(cLog, cErr) {
-  _getStoriesPromise()
-    .then(stories => {
-      const sentencesPromises = stories.map(story => {
-        return _getSentencesPromise(story.id);
-      });
-
-      return Promise.all(sentencesPromises);
-    })
-    .then(sentences => cLog(sentences))
-    .catch(err => cErr(err));
+  // TODO: Implement
 }
 
 const exercises = [
@@ -131,12 +87,3 @@ exercises.forEach((exercise, idx) => {
 
   exercise(cLog, cErr);
 });
-
-// FIXME: This doesn't really work in practice because of the async nature of
-// these exercises
-//
-// exercises.forEach((fn, idx) => {
-//   console.log(`*** Exercise ${idx} ***`);
-//   fn();
-//   console.log();
-// });
---
+++ solution.js
@@ -0,0 +1,133 @@
+const superagent = require('superagent');
+
+const endpoints = {
+  stories:   'http://liveproxy-rails-example.herokuapp.com/api/v1/stories',
+  sentences: 'http://liveproxy-rails-example.herokuapp.com/api/v1/sentences',
+}
+
+function _getStoriesPromise() {
+  return new Promise((resolve, reject) => {
+    superagent.get(endpoints.stories).end((err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(resp.body.stories);
+      }
+    });
+  });
+}
+
+function _getSentencesPromise(storyId) {
+  return new Promise((resolve, reject) => {
+    superagent.get(endpoints.sentences).query({ story_id: storyId }).end((err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(resp.body.sentences);
+      }
+    });
+  });
+}
+
+function __print(fn, exerciseNum, content) {
+  fn(`*** Exercise ${exerciseNum} ***`);
+  fn(content);
+  console.log();
+}
+
+function _log(exerciseNum, content) {
+  __print(console.log, exerciseNum, content);
+}
+
+function _error(exerciseNum, content) {
+  __print(console.error, exerciseNum, content);
+}
+
+// Print stories response using callbacks
+function callbackStories(cLog, cErr) {
+  superagent.get(endpoints.stories).end((err, resp) => {
+    if (err) {
+      cErr(err);
+    }
+
+    cLog(resp.body.stories);
+  });
+}
+
+// Print the first story's sentences using callbacks
+function callbackFirstStorySentences(cLog, cErr) {
+  superagent.get(endpoints.stories).end((err, resp) => {
+    if (err) {
+      cErr(err);
+    }
+    const stories = resp.body.stories;
+
+    superagent.get(endpoints.sentences).query({ story_id: stories[0].id }).end((err, resp) => {
+      if (err) {
+        cErr(err);
+      }
+
+      cLog(resp.body.sentences);
+    });
+  });
+}
+
+// Print stories response using promsies
+function promiseStories(cLog, cErr) {
+  _getStoriesPromise()
+    .then(res => cLog(res))
+    .catch(err => cErr(err));
+}
+
+// Print the first story's sentences using promises (naive implementation)
+function promiseFirstStorySentences(cLog, cErr) {
+  _getStoriesPromise()
+    .then(stories => {
+      const firstStory = stories[0];
+      _getSentencesPromise(firstStory.id)
+        .then(sentences => cLog(sentences))
+        .catch(err => cErr(err));
+    })
+    .catch(err => cErr(err));
+}
+
+// Print the first story's sentences using promises (flattened implementation)
+function arePromisesReallyBetterThanCallbacks(cLog, cErr) {
+  _getStoriesPromise()
+    .then(stories => {
+      const firstStory = stories[0];
+      return _getSentencesPromise(firstStory.id);
+    })
+    .then(sentences => cLog(sentences))
+    .catch(err => cErr(err));
+}
+
+// Print all the stories' sentences
+function promisesAllStoriesSentences(cLog, cErr) {
+  _getStoriesPromise()
+    .then(stories => {
+      const sentencesPromises = stories.map(story => {
+        return _getSentencesPromise(story.id);
+      });
+
+      return Promise.all(sentencesPromises);
+    })
+    .then(sentences => cLog(sentences))
+    .catch(err => cErr(err));
+}
+
+const exercises = [
+  callbackStories,
+  callbackFirstStorySentences,
+  promiseStories,
+  promiseFirstStorySentences,
+  arePromisesReallyBetterThanCallbacks,
+  promisesAllStoriesSentences,
+];
+
+exercises.forEach((exercise, idx) => {
+  const cLog = content => _log(idx, content);
+  const cErr = content => _error(idx, content);
+
+  exercise(cLog, cErr);
+});
